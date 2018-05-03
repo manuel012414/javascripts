d2c53c383ffcb652fb7c956cdd880be283d88667
manuel <jcanazag79@elpuig.xeill.net>
2018-05-03 16:25:45 +0200

    nuevo

---
+++ 002-loops.js
@@ -0,0 +1,32 @@
+var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+function greaterThan18(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+document.getElementById("A").innerHTML = greaterThan18(elements);
+
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares
+function greaterThan18Odd(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+document.getElementById("B").innerHTML = greaterThan18Odd(elements);
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares y que se encuentran en posiciones impares del vector
+function greaterThan18OddEvenPositions(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+
+document.getElementById("C").innerHTML = greaterThan18OddEvenPositions(elements);
